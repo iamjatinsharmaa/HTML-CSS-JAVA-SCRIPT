71490783596f787a73fd65ec67475d89f7da27ec
iamjatinsharmaa <jatin392@gmail.com>
2023-01-05 10:11:38 +0530

    Html, css& javascript

---
+++ Complete Web Development/tut59/mod.js
@@ -0,0 +1,17 @@
+console.log("This is module");
+
+function average(arr){
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element;
+    });
+    return sum/arr.length;
+}
+
+// module.exports = {
+//     avg: average,
+//     name: "Jatin",
+//     repo: "GitHub"
+// }
+
+module.exports.name = "Jatin";
